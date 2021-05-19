a82e746df08622b29a13288f01d91c3f9ffb0619
Nicolas CHEVILLARD <nicolas.chevillard@campus-igs-toulouse.fr>
2021-05-19 17:28:31 +0200

    Corrections pour supprimer les dossiers Views et Controller

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 using ProjetFinal_MAUREL_CHEVILLARD.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetFinal_MAUREL_CHEVILLARD
 {
@@ -25,7 +25,7 @@ namespace ProjetFinal_MAUREL_CHEVILLARD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
 
             services.AddDbContext<ProjetFinal_MAUREL_CHEVILLARDContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ProjetFinal_MAUREL_CHEVILLARDContext")));
@@ -40,10 +40,11 @@ namespace ProjetFinal_MAUREL_CHEVILLARD
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -53,9 +54,7 @@ namespace ProjetFinal_MAUREL_CHEVILLARD
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
