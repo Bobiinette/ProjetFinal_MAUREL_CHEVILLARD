632858d3bbb30579ad39d5cbb64afa49c1dbefb6
Nicolas MAUREL <nicolas.maurel2@campus-igs-toulouse.fr>
2021-05-19 16:26:54 +0200

    creation Freelance class

---
+++ Models/Freelance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetFinal_MAUREL_CHEVILLARD.Models
+{
+    public class Freelance
+    {
+        [Key]
+        public long Id { get; set; }
+        public String ResidenceCountry { get; set; }
+        public String WorkCountry { get; set; }
+
+        public int DayPrice { get; set; }
+
+        public int HourPrice { get; set; }
+
+        public int MonthPrice { get; set; }
+
+        public int TurnOver { get; set; }
+
+        public bool NotKnowPrice { get; set; }
+
+        public int NetSalary { get; set; }
+
+        public int BrutSalary { get; set; }
+
+        public int MonthDuration { get; set; }
+
+        public int DayByMonthDuration { get; set; }
+
+        public bool LessOneMonthDuration { get; set; }
+
+        public String Civility { get; set; }
+
+        public String Lastname { get; set; }
+
+        public String Firstname { get; set; }
+
+        public String Email { get; set; }
+
+        public String Telephone { get; set; }
+
+        public bool ConfidentialityPoliticAccepted { get; set; }
+
+        public bool MarketingOfferAccepted { get; set; }
+
+        public DateTime SimulationDate { get; set; }
+
+
+        public Freelance()
+        {
+
+        }
+    }
+}
