724322949a35f2cdbd2264ef83a4344e11f43767
Nicolas CHEVILLARD <nicolas.chevillard@campus-igs-toulouse.fr>
2021-05-19 14:50:23 +0200

    Premier commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetFinal_MAUREL_CHEVILLARD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
