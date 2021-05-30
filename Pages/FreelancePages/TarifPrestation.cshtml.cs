6bec32c74d6b65fc6ccec48908423f0614ea34c1
Nicolas MAUREL <nicolas.maurel2@campus-igs-toulouse.fr>
2021-05-30 21:07:10 +0200

    Ajout Page tarif

---
+++ Pages/FreelancePages/TarifPrestation.cshtml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjetFinal_MAUREL_CHEVILLARD.Models;
+
+namespace ProjetFinal_MAUREL_CHEVILLARD.Pages
+{
+    public class TarifPrestationModel : PageModel
+    {
+
+        private readonly ProjetFinal_MAUREL_CHEVILLARD.Data.ProjetFinal_MAUREL_CHEVILLARDContext _context;
+
+        public TarifPrestationModel(ProjetFinal_MAUREL_CHEVILLARD.Data.ProjetFinal_MAUREL_CHEVILLARDContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Freelance Freelance { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Freelance.Add(Freelance);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+    }
+
+    public class CreateModel : PageModel
+    {
+       
+    }
+
+}
